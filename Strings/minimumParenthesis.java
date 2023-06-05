89974eb05383dd7372c9122cbffa84ccedfea33f
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2023-06-05 23:55:49 +0530

    Update minimumParenthesis.java

--- Strings/minimumParenthesis.java
+++ Strings/minimumParenthesis.java
@@ -1,4 +1,34 @@
 package Strings;
+import java.util.*;
 
 public class minimumParenthesis {
+    public static void main(String args[]){
+        String s="))(()";
+        int min=minimum_Parenthesis(s);
+        System.out.println("The minimum parenthesis is :- "+ min);
+
+    }
+    static int minimum_Parenthesis(String s){
+        int n=s.length();
+        Stack<Integer> stk=new Stack<>();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='(')
+                stk.push(1);
+            else{
+                if(!stk.isEmpty()){
+                    stk.pop();
+                }
+                else{
+                    cnt++;
+                }
+            }
+
+        }
+        cnt+=stk.size();
+        return cnt;
+
+    }
+
+
 }
